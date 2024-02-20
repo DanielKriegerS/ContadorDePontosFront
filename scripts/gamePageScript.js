c17969b0fb518dd63acec86504a3779d9742790c
dschifel <danielschifelbain@gmail.com>
2024-02-20 18:44:49 -0300

    Created logic to pass the hand, count points and finish the game (return to main)

--- scripts/gamePageScript.js
+++ scripts/gamePageScript.js
@@ -12,6 +12,8 @@ const playerTwoPontSpan = document.getElementById("playerTwoPont");
 playerOneNick.innerHTML = localStorage.getItem("playerOne");
 playerTwoNick.innerHTML = localStorage.getItem("playerTwo");
 
+var playerWithHand = -1; 
+
 var playerOnePont = 0;
 var playerTwoPont = 0;
 
@@ -20,6 +22,30 @@ playerTwoPontSpan.innerHTML = playerTwoPont;
 
 const playerOneBtn = document.getElementById("playerOneBtn");
 const playerTwoBtn = document.getElementById("playerTwoBtn");
+
+const playerOneDiv = document.getElementById("playerOneDiv");
+const playerTwoDiv = document.getElementById("playerTwoDiv");
+
+const changePlayerHand = () => {
+    const divOne = document.getElementById("divOne");
+    const divTwo = document.getElementById("divTwo");
+
+    if(playerWithHand == 0 || playerWithHand == -1) {
+        divOne.style.visibility = "visible";
+        divTwo.style.visibility = "hidden";
+        playerWithHand = 1;
+        playerTwoDiv.classList.remove("hasHand");
+        playerOneDiv.classList.add("hasHand");
+    } else {
+        divTwo.style.visibility = "visible";
+        divOne.style.visibility = "hidden";
+        playerWithHand = 0;
+        playerOneDiv.classList.remove("hasHand");
+        playerTwoDiv.classList.add("hasHand");
+    }
+}
+
+changePlayerHand();
 
 playerOneBtn.addEventListener("click", () => {
     this.playerOnePont += 1;
@@ -39,9 +65,9 @@ const testEndGame = () => {
     var actualPontPOne = localStorage.getItem("playerOnePont");
     var actualPontPTwo = localStorage.getItem("playerTwoPont");
 
-
     if(actualPontPOne == maxPont || actualPontPTwo == maxPont) {
         window.location.href = '/index.html'
-    }
-    
+    }   
+
+    changePlayerHand();
 }
