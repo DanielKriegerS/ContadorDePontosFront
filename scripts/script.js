2ff74d8f79c69d9de6cea5774c2341374d8d9fd6
dschifel <danielschifelbain@gmail.com>
2024-03-04 14:30:16 -0300

    Maked some adjusted to best reflect the idea of project

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 const newGameButton = document.getElementById("newGame");
+const leaderboardButton = document.getElementById("leaderboard");
 
 newGameButton.addEventListener("click", () => {
     window.location.href = "./pages/config.html";
 });
+
+leaderboardButton.addEventListener("click", () => {
+    window.location.href = "./pages/endedGames.html";
+})
