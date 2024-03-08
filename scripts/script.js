fd465a21ac8075e4c0ac5532e4b0b0398b6d10d6
dschifel <danielschifelbain@gmail.com>
2024-03-08 10:11:46 -0300

    adjusted to include registry page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const newGameButton = document.getElementById("newGame");
 const leaderboardButton = document.getElementById("leaderboard");
+const registryButton = document.getElementById("registryBtn");
 
 newGameButton.addEventListener("click", () => {
     window.location.href = "./pages/config.html";
@@ -7,4 +8,8 @@ newGameButton.addEventListener("click", () => {
 
 leaderboardButton.addEventListener("click", () => {
     window.location.href = "./pages/endedGames.html";
+})
+
+registryButton.addEventListener("click", () => {
+    window.location.href = "./pages/registry.html";
 })
